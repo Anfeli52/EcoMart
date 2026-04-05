43c3dc54faca85b60f884eed4bedb47642daae9f
andresdiazds3 <andresjimenezdiaz123@gmail.com>
2026-04-05 00:03:20 -0500

    feature: inicializacion de estructura general del backend, estructura de carpetas, instalacion de dependencias, y primer migracion de la base con prisma client

---
+++ Backend/src/server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+//middelware
+app.use(cors())
+app.use(express.json())
+
+//Aqui se montaran las rutas
+//app.use('/user', userRoutes)
+
+//creamos el servidor
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`)
+})
